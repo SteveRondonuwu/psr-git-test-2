7e558402253cc518c424ed389bfd881e0cc44e41
SteveRondonuwu <steverondonuwu33@gmail.com>
2021-08-20 10:23:45 +0700

    menambahkan paragraf

--- index.php
+++ index.php
@@ -8,5 +8,6 @@
 </head>
 <body>
     <h1>Hello World</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse, beatae sequi? Illo, quia? Consequuntur hic quaerat, in adipisci nam quas aut modi. Molestias facilis sunt qui amet molestiae nesciunt ex!</p>
 </body>
 </html>
