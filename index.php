271f0dca839ea2ebbc17e78cfeca4bb085c750d9
SteveRondonuwu <steverondonuwu33@gmail.com>
2021-08-20 10:30:02 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -9,5 +9,9 @@
 <body>
     <h1>Hello World</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse, beatae sequi? Illo, quia? Consequuntur hic quaerat, in adipisci nam quas aut modi. Molestias facilis sunt qui amet molestiae nesciunt ex!</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
